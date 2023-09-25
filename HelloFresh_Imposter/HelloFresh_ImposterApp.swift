443e41061a848c65438b6b5fba0a9f747e3a08b8
Kateryna Nerush <kate.nerush@hotmail.com>
2023-09-25 18:11:05 +0200

    Initial Commit

---
+++ HelloFresh_Imposter/HelloFresh_ImposterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFresh_ImposterApp.swift
+//  HelloFresh_Imposter
+//
+//  Created by Kateryna Nerush on 25.09.23.
+//
+
+import SwiftUI
+
+@main
+struct HelloFresh_ImposterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
